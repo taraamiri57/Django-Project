884ddf2df182d805f9e92f945bdcbeb17b73ad9c
Tara <t2amiri@uwaterloo.ca>
2023-04-26 13:09:01 -0400

    Step 2 Part (a) Implementation
    Impelemented unique question text

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200, unique = True)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
