132e38d179c0ea70eb6086565b5239500b9bccbe
Tara <t2amiri@uwaterloo.ca>
2023-04-26 15:14:36 -0400

    Step 2 Part (b) Implementation
    Impelemented unique choice text

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,11 +10,12 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200, unique=True)
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
